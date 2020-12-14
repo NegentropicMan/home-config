c4b56a20f6ab3c5919457652199b64b891e6c9c6
NegentropicMan <florian.schmitz@gmail.com>
2020-12-14 20:28:53 +0100

    Added various stuff

--- role/remoteenv.nix
+++ role/remoteenv.nix
@@ -1,8 +1,19 @@
 { config, pkgs, ... }:
 
+let semantic-highlight= pkgs.vimUtils.buildVimPlugin {
+      name = "semantic-highlight";
+      src = pkgs.fetchFromGitHub {
+        owner = "jaxbot";
+        repo = "semantic-highlight.vim.git";
+        rev = "8537920706fafab12a2c2a138dfb6f5c35a6b240";
+        sha256 = "04975ly6zd58zhs2v07dw6m1dha0gkmfm0pvhlrgcrf2qp8n4m3v";
+      };
+  };
+in 
 {
   home.packages = with pkgs; [
     htop
+    exa
     fortune
     broot
     screen
@@ -21,16 +32,20 @@
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
-
     extraConfig = ''
-      colorscheme PaperColor
       set relativenumber
+      colorscheme PaperColor
+      set nobackup
+      set showcmd
+      let mapleader=","
+      nnoremap <Leader>s :SemanticHighlightToggle<cr>
     '';
 
     plugins = with pkgs.vimPlugins; [
       airline
       papercolor-theme
       vim-addon-nix
+      semantic-highlight
     ];
   };
 
@@ -43,11 +58,10 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      ll = "ls -al";
       ".." = "cd ..";
       "a.sharetext" = "curl -F 'f:1=<-' ix.io";
       "a.weather" = "curl wttr.in/Nürnberg";
-    
+      exa = "ls"; 
     };
     
     profileExtra = pkgs.lib.fileContents ./profile.bash;
