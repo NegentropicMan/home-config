c620525bfb835427476d236aa1182dcbeadaeecd
NegentropicMan <florian.schmitz@gmail.com>
2021-04-21 22:31:08 +0200

    Removed vscode

--- Roles/graphical.nix
+++ Roles/graphical.nix
@@ -5,13 +5,14 @@
     ./Apps/fonts.nix
     ./Apps/sway.nix
     ./Apps/firefox.nix
-    ./Apps/vscode.nix
+    #./Apps/vscode.nix
   ];
 
   home.packages = with pkgs; [
     zathura
     cantata
     dropbox
+    qutebrowser
   ];
 
   home.sessionVariables."_JAVA_AWT_WM_NONREPARENTING" = "1"; # For Java/AWT programs which do not like wayland
