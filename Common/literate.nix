690c99be850167223d47fafda6ba086fbfd8869a
NegentropicMan <florian.schmitz@gmail.com>
2021-05-06 21:57:56 +0200

    Added literate

--- Common/literate.nix
+++ Common/literate.nix
@@ -2,23 +2,27 @@ with import <nixpkgs> {};
 
 { config, pkgs, options, ... } :
 let
-  format-udf = stdenv.mkDerivation rec {
-  # String interpolation to include the version number in the name
-  # Including a version in the name is idiomatic
-  pname = "literate";
-  version = "git-2021-05-05";
+  literate = stdenv.mkDerivation rec {
+    # String interpolation to include the version number in the name
+    # Including a version in the name is idiomatic
+    pname = "literate";
+    version = "git-2021-05-06";
 
-  # fetchurl is a build support again; and does some funky stuff to support
-  # selecting from a predefined set of mirrors
-  src = fetchFromGitHub {
-    owner = "zyedidia";
-    repo = "literate";
-    rev = "7004dffec0cff3068828514eca72172274fd3f7d";
-    sha256 = "0iskwv05kbf2snk1ggh95mwn9q16r764n967cvfs4arp8cmmrhkk";
-  };
-  
-  buildInputs = [ utillinux ];
-  };
+    name = "literate";
+    src = fetchFromGitHub {
+      owner = "zyedidia";
+      repo = "Literate";
+      rev = "7004dffec0cff3068828514eca72172274fd3f7d";
+      sha256 = "0x4xgrdskybaa7ssv81grmwyc1k167v3nwj320jvp5l59xxlbcvs";
+      fetchSubmodules = true;
+    };
+    buildInputs = [ dub dmd ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp ./bin/* $out/bin/
+    '';
+  };  
 in
 {
   home.packages = with pkgs; [ literate ];
